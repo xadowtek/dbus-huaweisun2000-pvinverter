5b5e0d9245f28dc737bb46cf434635daa9b69a32
xadow <72521329+xadowtek@users.noreply.github.com>
2025-08-13 18:08:56 +0200

    Update datatypes.py

--- sun2000_modbus/datatypes.py
+++ sun2000_modbus/datatypes.py
@@ -2,58 +2,67 @@ from enum import Enum
 
 
 class DataType(Enum):
-    STRING = "string"
-    UINT16_BE = "uint16"
-    UINT32_BE = "uint32"
-    INT16_BE = "int16"
-    INT32_BE = "int32"
-    BITFIELD16 = "bitfield16"
-    BITFIELD32 = "bitfield32"
-    MULTIDATA = "multidata"
+    STRING = 'string'
+    UINT16_BE = 'uint16'
+    UINT32_BE = 'uint32'
+    INT16_BE = 'int16'
+    INT32_BE = 'int32'
+    BITFIELD16 = 'bitfield16'
+    BITFIELD32 = 'bitfield32'
+    MULTIDATA = 'multidata'
 
 
 def decode_string(value):
-    return value.decode("utf-8", "replace").strip("\0")
+    return value.decode('utf-8', 'replace').strip('\0')
+
+
+def encode_uint_be(value, length):
+    return int.to_bytes(value, length=length, byteorder='big', signed=False)
 
 
 def decode_uint_be(value):
-    return int.from_bytes(value, byteorder="big", signed=False)
+    return int.from_bytes(value, byteorder='big', signed=False)
+
+
+def encode_int_be(value, length):
+    return int.to_bytes(value, length=length, byteorder='big', signed=True)
 
 
 def decode_int_be(value):
-    return int.from_bytes(value, byteorder="big", signed=True)
+    return int.from_bytes(value, byteorder='big', signed=True)
 
 
 def decode_bitfield(value):
     return ''.join(format(byte, '08b') for byte in value)
 
 
+def encode(value, data_type):
+    if data_type == DataType.UINT16_BE:
+        return encode_uint_be(value, 2)
+    elif data_type == DataType.UINT32_BE:
+        return encode_uint_be(value, 4)
+    elif data_type == DataType.INT16_BE:
+        return encode_int_be(value, 2)
+    elif data_type == DataType.INT32_BE:
+        return encode_int_be(value, 4)
+    elif data_type == DataType.MULTIDATA:
+        if len(value) % 2 != 0:
+            raise ValueError('Multidata value length must be a multiple of 2')
+        return value
+    else:
+        raise ValueError('Writing is not supported for register type')
+
+
 def decode(value, data_type):
     if data_type == DataType.STRING:
         return decode_string(value)
-    elif data_type in [DataType.UINT16_BE, DataType.UINT32_BE]:
+    elif data_type == DataType.UINT16_BE or data_type == DataType.UINT32_BE:
         return decode_uint_be(value)
-    elif data_type in [DataType.INT16_BE, DataType.INT32_BE]:
+    elif data_type == DataType.INT16_BE or data_type == DataType.INT32_BE:
         return decode_int_be(value)
-    elif data_type in [DataType.BITFIELD16, DataType.BITFIELD32]:
+    elif data_type == DataType.BITFIELD16 or data_type == DataType.BITFIELD32:
         return decode_bitfield(value)
     elif data_type == DataType.MULTIDATA:
         return value
     else:
-        raise ValueError("Unknown register type")
-
-
-# def decode(value, data_type):
-#     match data_type:
-#         case DataType.STRING:
-#             return decode_string(value)
-#         case DataType.UINT16_BE | DataType.UINT32_BE:
-#             return decode_uint_be(value)
-#         case DataType.INT16_BE | DataType.INT32_BE:
-#             return decode_int_be(value)
-#         case DataType.BITFIELD16 | DataType.BITFIELD32:
-#             return decode_bitfield(value)
-#         case DataType.MULTIDATA:
-#             return value
-#         case _:
-#             raise ValueError("Unknown register type")
+        raise ValueError('Unknown register type')
