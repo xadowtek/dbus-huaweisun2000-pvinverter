b7d562e81a47ca22f4719c807448ac261733a805
xadow <72521329+xadowtek@users.noreply.github.com>
2025-08-13 18:16:23 +0200

    Update inverter.py

--- sun2000_modbus/inverter.py
+++ sun2000_modbus/inverter.py
@@ -1,55 +1,64 @@
 import logging
 import time
 
-from pymodbus.client.sync import ModbusTcpClient
+from pymodbus.client import ModbusTcpClient
 from pymodbus.exceptions import ModbusIOException, ConnectionException
 
 from . import datatypes
+from .registers import AccessType
 
-logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+log_format = logging.Formatter('%(levelname)s:%(name)s:%(message)s')
+handler = logging.StreamHandler()
+handler.setFormatter(log_format)
+logger.addHandler(handler)
+logger.propagate = False
 
 
 class Sun2000:
-    def __init__(self, host, port=502, timeout=5, wait=2, modbus_unit=0):
+    def __init__(self, host, port=502, timeout=5, wait=2, slave=0): # some models need slave=1
         self.wait = wait
-        self.modbus_unit = modbus_unit
-        self.inverter = ModbusTcpClient(host, port, timeout=timeout)
+        self.slave = slave
+        self.inverter = ModbusTcpClient(host=host, port=port, timeout=timeout)
 
     def connect(self):
         if not self.isConnected():
             self.inverter.connect()
             time.sleep(self.wait)
             if self.isConnected():
-                logging.info('Successfully connected to inverter')
+                logger.info('Successfully connected to inverter')
                 return True
             else:
-                logging.error('Connection to inverter failed')
+                logger.error('Connection to inverter failed')
                 return False
-        else:
-            return True
 
     def disconnect(self):
         """Close the underlying tcp socket"""
         # Some Sun2000 models with the SDongle WLAN-FE require the TCP connection to be closed
-        # as soon as possible. Leaving the TCP connection open for an extended time may cause 
-        # dongle reboots and/or FusionSolar portal updates to be delayed or even paused. 
+        # as soon as possible. Leaving the TCP connection open for an extended time may cause
+        # dongle reboots and/or FusionSolar portal updates to be delayed or even paused.
         self.inverter.close()
 
     def isConnected(self):
         """Check if underlying tcp socket is open"""
         return self.inverter.is_socket_open()
 
+    @property
+    def connected(self):
+        return self.isConnected()
+
     def read_raw_value(self, register):
         if not self.isConnected():
             raise ValueError('Inverter is not connected')
 
         try:
-            register_value = self.inverter.read_holding_registers(register.value.address, register.value.quantity, unit=self.modbus_unit)
+            register_value = self.inverter.read_holding_registers(address=register.value.address, count=register.value.quantity, slave=self.slave)
             if type(register_value) == ModbusIOException:
-                logging.error("Inverter modbus unit did not respond")
+                logger.error('Inverter unit did not respond')
                 raise register_value
         except ConnectionException:
-            logging.error("A connection error occurred")
+            logger.error('A connection error occurred')
             raise
 
         return datatypes.decode(register_value.encode()[1:], register.value.data_type)
@@ -62,11 +71,14 @@ class Sun2000:
         else:
             return raw_value / register.value.gain
 
-    def read_formatted(self, register):
+    def read_formatted(self, register, use_locale=False):
         value = self.read(register)
 
         if register.value.unit is not None:
-            return f'{value} {register.value.unit}'
+            if use_locale:
+                return f'{value:n} {register.value.unit}'
+            else:
+                return f'{value} {register.value.unit}'
         elif register.value.mapping is not None:
             return register.value.mapping.get(value, 'undefined')
         else:
@@ -74,11 +86,11 @@ class Sun2000:
 
     def read_range(self, start_address, quantity=0, end_address=0):
         if quantity == 0 and end_address == 0:
-            raise ValueError("Either parameter quantity or end_address is required and must be greater than 0")
+            raise ValueError('Either parameter quantity or end_address is required and must be greater than 0')
         if quantity != 0 and end_address != 0:
-            raise ValueError("Only one parameter quantity or end_address should be defined")
+            raise ValueError('Only one parameter quantity or end_address should be defined')
         if end_address != 0 and end_address <= start_address:
-            raise ValueError("end_address must be greater than start_address")
+            raise ValueError('end_address must be greater than start_address')
 
         if not self.isConnected():
             raise ValueError('Inverter is not connected')
@@ -86,12 +98,30 @@ class Sun2000:
         if end_address != 0:
             quantity = end_address - start_address + 1
         try:
-            register_range_value = self.inverter.read_holding_registers(start_address, quantity, unit=self.modbus_unit)
+            register_range_value = self.inverter.read_holding_registers(address=start_address, count=quantity, slave=self.slave)
             if type(register_range_value) == ModbusIOException:
-                logging.error("Inverter modbus unit did not respond")
+                logger.error('Inverter unit did not respond')
                 raise register_range_value
         except ConnectionException:
-            logging.error("A connection error occurred")
+            logger.error('A connection error occurred')
             raise
 
         return datatypes.decode(register_range_value.encode()[1:], datatypes.DataType.MULTIDATA)
+
+    def write(self, register, value):
+        if not self.isConnected():
+            raise ValueError('Inverter is not connected')
+        if not register.value.access_type in [AccessType.RW, AccessType.WO]:
+            raise ValueError('Register is not writeable')
+
+        encoded_value = datatypes.encode(value, register.value.data_type)
+        chunks = [int.from_bytes(encoded_value[i:i+2], byteorder='big', signed=False) for i in range(0, len(encoded_value), 2)]
+
+        try:
+            response = self.inverter.write_registers(address=register.value.address, values=chunks, slave=self.slave)
+            if type(response) == ModbusIOException:
+                logger.error('Inverter unit did not respond')
+                raise response
+        except ConnectionException:
+            logger.error('A connection error occurred')
+            raise
